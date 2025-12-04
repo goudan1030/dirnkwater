7abd6171ce78b1005d960eca3054204a6f3a0c2d
你的GitHub用户名 <GitHub>
2025-12-04 16:34:15 +0800

    fix: 修复 water.js 中 WaterRecord 模型导入路径错误

--- server/routes/water.js
+++ server/routes/water.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { WaterRecord } = require('../models/WaterRecord')
+const { WaterRecord } = require('../models/User')
 const { authenticateToken } = require('../utils/jwt')
 
 /**
